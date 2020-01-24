8eedeecb2221f5975315ab563cebcb10fe88ed8b
ColeDrain <cole.drain@yahoo.com>
2020-01-24 13:43:46 +0100

    My LastPrice app, eleventh commit

--- lastprice/settings.py
+++ lastprice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$(v(jr&53_hb3nnb@9x6f9t*cmh=u5ybtj!=-#9d!1tj3l$n(@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
