29c8857b16941079f5f862a6550971d898ca0946
ColeDrain <cole.drain@yahoo.com>
2020-01-24 07:37:10 +0100

    My LastPrice app, fifth commit

--- lastprice/settings.py
+++ lastprice/settings.py
@@ -119,12 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
